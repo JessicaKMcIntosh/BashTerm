7261edfb0c73e763fe8202c5add17189c61546c0
Jessica K McIntosh <>
2025-12-30 14:43:38 -0800

    Shortcuts are now generated from an associative array.

--- color.sh
+++ color.sh
@@ -59,36 +59,10 @@ _TERM_COLOR_ALIASES=(
 for color in "${!_TERM_COLOR_ALIASES[@]}"; do
     _TERM_COLORS["${color}"]="${_TERM_COLORS[${_TERM_COLOR_ALIASES[$color]}]}"
 done
+
 # Some handy shortcuts for less typing.
-export TERM_BG_BLACK="${TERM_BG[black]}"
-export TERM_BG_BLUE="${TERM_BG[blue]}"
-export TERM_BG_BRIGHT_BLACK="${TERM_BG[brightblack]}"
-export TERM_BG_BRIGHT_BLUE="${TERM_BG[brightblue]}"
-export TERM_BG_BRIGHT_CYAN="${TERM_BG[brightcyan]}"
-export TERM_BG_BRIGHT_GREEN="${TERM_BG[brightgreen]}"
-export TERM_BG_BRIGHT_MAGENTA="${TERM_BG[brightmagenta]}"
-export TERM_BG_BRIGHT_RED="${TERM_BG[brightred]}"
-export TERM_BG_BRIGHT_WHITE="${TERM_BG[brightwhite]}"
-export TERM_BG_BRIGHT_YELLOW="${TERM_BG[brightyellow]}"
-export TERM_BG_CYAN="${TERM_BG[cyan]}"
-export TERM_BG_GREEN="${TERM_BG[green]}"
-export TERM_BG_MAGENTA="${TERM_BG[magenta]}"
-export TERM_BG_RED="${TERM_BG[red]}"
-export TERM_BG_WHITE="${TERM_BG[white]}"
-export TERM_BG_YELLOW="${TERM_BG[yellow]}"
-export TERM_FG_BLACK="${TERM_FG[black]}"
-export TERM_FG_BLUE="${TERM_FG[blue]}"
-export TERM_FG_BRIGHT_BLACK="${TERM_FG[brightblack]}"
-export TERM_FG_BRIGHT_BLUE="${TERM_FG[brightblue]}"
-export TERM_FG_BRIGHT_CYAN="${TERM_FG[brightcyan]}"
-export TERM_FG_BRIGHT_GREEN="${TERM_FG[brightgreen]}"
-export TERM_FG_BRIGHT_MAGENTA="${TERM_FG[brightmagenta]}"
-export TERM_FG_BRIGHT_RED="${TERM_FG[brightred]}"
-export TERM_FG_BRIGHT_WHITE="${TERM_FG[brightwhite]}"
-export TERM_FG_BRIGHT_YELLOW="${TERM_FG[brightyellow]}"
-export TERM_FG_CYAN="${TERM_FG[cyan]}"
-export TERM_FG_GREEN="${TERM_FG[green]}"
-export TERM_FG_MAGENTA="${TERM_FG[magenta]}"
-export TERM_FG_RED="${TERM_FG[red]}"
-export TERM_FG_WHITE="${TERM_FG[white]}"
-export TERM_FG_YELLOW="${TERM_FG[yellow]}"
+for color in "${_TERM_COLORS[@]}"; do
+    declare -x "TERM_FG_${color^^}=${TERM_FG[$color]}"
+    declare -x "TERM_BG_${color^^}=${TERM_BG[$color]}"
+done
+
