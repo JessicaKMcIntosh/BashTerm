7f1790dd704d834349d5dbb7fefebd3387ef95d8
Jessica K McIntosh <>
2026-01-19 16:54:50 -0800

    Generate the color codes to test against.
    This is to be consistent with how the other attributes are being tested.

--- tests/printf.sh
+++ tests/printf.sh
@@ -37,7 +37,6 @@ export TEST_TITLE="Test 'printf.sh'."
 
 # Any setup that may need to be performed.
 test_setup(){
-    # The xterm escape codes to test against.
     test_attr_bold=$'\E[1m'
     test_attr_clear=$'\E[H\E[2J\E[3J'
     test_attr_cnorm=$'\E[?12l\E[?25h'
@@ -56,11 +55,44 @@ test_setup(){
     test_attr_rev=$'\E[7m'
     test_attr_smso=$'\E[7m'
     test_attr_smul=$'\E[4m'
+
+    test_color_fg_black="$(tput setaf 0)"
+    test_color_fg_red="$(tput setaf 1)"
+    test_color_fg_green="$(tput setaf 2)"
+    test_color_fg_yellow="$(tput setaf 3)"
+    test_color_fg_blue="$(tput setaf 4)"
+    test_color_fg_magenta="$(tput setaf 5)"
+    test_color_fg_cyan="$(tput setaf 6)"
+    test_color_fg_white="$(tput setaf 7)"
+    test_color_fg_brightblack="$(tput setaf 8)"
+    test_color_fg_brightred="$(tput setaf 9)"
+    test_color_fg_brightgreen="$(tput setaf 10)"
+    test_color_fg_brightyellow="$(tput setaf 11)"
+    test_color_fg_brightblue="$(tput setaf 12)"
+    test_color_fg_brightmagenta="$(tput setaf 13)"
+    test_color_fg_brightcyan="$(tput setaf 14)"
+    test_color_fg_brightwhite="$(tput setaf 15)"
+
+    test_color_bg_black="$(tput setab 0)"
+    test_color_bg_red="$(tput setab 1)"
+    test_color_bg_green="$(tput setab 2)"
+    test_color_bg_yellow="$(tput setab 3)"
+    test_color_bg_blue="$(tput setab 4)"
+    test_color_bg_magenta="$(tput setab 5)"
+    test_color_bg_cyan="$(tput setab 6)"
+    test_color_bg_white="$(tput setab 7)"
+    test_color_bg_brightblack="$(tput setab 8)"
+    test_color_bg_brightred="$(tput setab 9)"
+    test_color_bg_brightgreen="$(tput setab 10)"
+    test_color_bg_brightyellow="$(tput setab 11)"
+    test_color_bg_brightblue="$(tput setab 12)"
+    test_color_bg_brightmagenta="$(tput setab 13)"
+    test_color_bg_brightcyan="$(tput setab 14)"
+    test_color_bg_brightwhite="$(tput setab 15)"
 }
 
 # Any cleanup that may need to be performed.
 test_cleanup(){
-    # Cleanup the testing variables.
     unset test_attr_bold
     unset test_attr_clear
     unset test_attr_dim
@@ -76,6 +108,40 @@ test_cleanup(){
     unset test_attr_rev
     unset test_attr_smso
     unset test_attr_smul
+
+    unset test_color_fg_black
+    unset test_color_fg_red
+    unset test_color_fg_green
+    unset test_color_fg_yellow
+    unset test_color_fg_blue
+    unset test_color_fg_magenta
+    unset test_color_fg_cyan
+    unset test_color_fg_white
+    unset test_color_fg_brightblack
+    unset test_color_fg_brightred
+    unset test_color_fg_brightgreen
+    unset test_color_fg_brightyellow
+    unset test_color_fg_brightblue
+    unset test_color_fg_brightmagenta
+    unset test_color_fg_brightcyan
+    unset test_color_fg_brightwhite
+
+    unset test_color_bg_black
+    unset test_color_bg_red
+    unset test_color_bg_green
+    unset test_color_bg_yellow
+    unset test_color_bg_blue
+    unset test_color_bg_magenta
+    unset test_color_bg_cyan
+    unset test_color_bg_white
+    unset test_color_bg_brightblack
+    unset test_color_bg_brightred
+    unset test_color_bg_brightgreen
+    unset test_color_bg_brightyellow
+    unset test_color_bg_brightblue
+    unset test_color_bg_brightmagenta
+    unset test_color_bg_brightcyan
+    unset test_color_bg_brightwhite
 }
 
 # Test basic functionality.
@@ -144,39 +210,39 @@ test::backslash(){
 
 # Test colors.
 test::color(){
-    assert_run 'term::printf "%(black)"' "0" "$(tput setaf 0)" "Check the foreground color black."
-    assert_run 'term::printf "%(red)"' "0" "$(tput setaf 1)" "Check the foreground color red."
-    assert_run 'term::printf "%(green)"' "0" "$(tput setaf 2)" "Check the foreground color green."
-    assert_run 'term::printf "%(yellow)"' "0" "$(tput setaf 3)" "Check the foreground color yellow."
-    assert_run 'term::printf "%(blue)"' "0" "$(tput setaf 4)" "Check the foreground color blue."
-    assert_run 'term::printf "%(magenta)"' "0" "$(tput setaf 5)" "Check the foreground color magenta."
-    assert_run 'term::printf "%(cyan)"' "0" "$(tput setaf 6)" "Check the foreground color cyan."
-    assert_run 'term::printf "%(white)"' "0" "$(tput setaf 7)" "Check the foreground color white."
-    assert_run 'term::printf "%(brightblack)"' "0" "$(tput setaf 8)" "Check the foreground color brightblack."
-    assert_run 'term::printf "%(brightred)"' "0" "$(tput setaf 9)" "Check the foreground color brightred."
-    assert_run 'term::printf "%(brightgreen)"' "0" "$(tput setaf 10)" "Check the foreground color brightgreen."
-    assert_run 'term::printf "%(brightyellow)"' "0" "$(tput setaf 11)" "Check the foreground color brightyellow."
-    assert_run 'term::printf "%(brightblue)"' "0" "$(tput setaf 12)" "Check the foreground color brightblue."
-    assert_run 'term::printf "%(brightmagenta)"' "0" "$(tput setaf 13)" "Check the foreground color brightmagenta."
-    assert_run 'term::printf "%(brightcyan)"' "0" "$(tput setaf 14)" "Check the foreground color brightcyan."
-    assert_run 'term::printf "%(brightwhite)"' "0" "$(tput setaf 15)" "Check the foreground color brightwhite."
+    assert_run 'term::printf "%(black)"' "0" "${test_color_fg_black}" "Check the foreground color black."
+    assert_run 'term::printf "%(red)"' "0" "${test_color_fg_red}" "Check the foreground color red."
+    assert_run 'term::printf "%(green)"' "0" "${test_color_fg_green}" "Check the foreground color green."
+    assert_run 'term::printf "%(yellow)"' "0" "${test_color_fg_yellow}" "Check the foreground color yellow."
+    assert_run 'term::printf "%(blue)"' "0" "${test_color_fg_blue}" "Check the foreground color blue."
+    assert_run 'term::printf "%(magenta)"' "0" "${test_color_fg_magenta}" "Check the foreground color magenta."
+    assert_run 'term::printf "%(cyan)"' "0" "${test_color_fg_cyan}" "Check the foreground color cyan."
+    assert_run 'term::printf "%(white)"' "0" "${test_color_fg_white}" "Check the foreground color white."
+    assert_run 'term::printf "%(brightblack)"' "0" "${test_color_fg_brightblack}" "Check the foreground color brightblack."
+    assert_run 'term::printf "%(brightred)"' "0" "${test_color_fg_brightred}" "Check the foreground color brightred."
+    assert_run 'term::printf "%(brightgreen)"' "0" "${test_color_fg_brightgreen}" "Check the foreground color brightgreen."
+    assert_run 'term::printf "%(brightyellow)"' "0" "${test_color_fg_brightyellow}" "Check the foreground color brightyellow."
+    assert_run 'term::printf "%(brightblue)"' "0" "${test_color_fg_brightblue}" "Check the foreground color brightblue."
+    assert_run 'term::printf "%(brightmagenta)"' "0" "${test_color_fg_brightmagenta}" "Check the foreground color brightmagenta."
+    assert_run 'term::printf "%(brightcyan)"' "0" "${test_color_fg_brightcyan}" "Check the foreground color brightcyan."
+    assert_run 'term::printf "%(brightwhite)"' "0" "${test_color_fg_brightwhite}" "Check the foreground color brightwhite."
 
-    assert_run 'term::printf "%(BLACK)"' "0" "$(tput setab 0)" "Check the background color black."
-    assert_run 'term::printf "%(RED)"' "0" "$(tput setab 1)" "Check the background color red."
-    assert_run 'term::printf "%(GREEN)"' "0" "$(tput setab 2)" "Check the background color green."
-    assert_run 'term::printf "%(YELLOW)"' "0" "$(tput setab 3)" "Check the background color yellow."
-    assert_run 'term::printf "%(BLUE)"' "0" "$(tput setab 4)" "Check the background color blue."
-    assert_run 'term::printf "%(MAGENTA)"' "0" "$(tput setab 5)" "Check the background color magenta."
-    assert_run 'term::printf "%(CYAN)"' "0" "$(tput setab 6)" "Check the background color cyan."
-    assert_run 'term::printf "%(WHITE)"' "0" "$(tput setab 7)" "Check the background color white."
-    assert_run 'term::printf "%(BRIGHTBLACK)"' "0" "$(tput setab 8)" "Check the background color brightblack."
-    assert_run 'term::printf "%(BRIGHTRED)"' "0" "$(tput setab 9)" "Check the background color brightred."
-    assert_run 'term::printf "%(BRIGHTGREEN)"' "0" "$(tput setab 10)" "Check the background color brightgreen."
-    assert_run 'term::printf "%(BRIGHTYELLOW)"' "0" "$(tput setab 11)" "Check the background color brightyellow."
-    assert_run 'term::printf "%(BRIGHTBLUE)"' "0" "$(tput setab 12)" "Check the background color brightblue."
-    assert_run 'term::printf "%(BRIGHTMAGENTA)"' "0" "$(tput setab 13)" "Check the background color brightmagenta."
-    assert_run 'term::printf "%(BRIGHTCYAN)"' "0" "$(tput setab 14)" "Check the background color brightcyan."
-    assert_run 'term::printf "%(BRIGHTWHITE)"' "0" "$(tput setab 15)" "Check the background color brightwhite."
+    assert_run 'term::printf "%(BLACK)"' "0" "${test_color_bg_black}" "Check the background color black."
+    assert_run 'term::printf "%(RED)"' "0" "${test_color_bg_red}" "Check the background color red."
+    assert_run 'term::printf "%(GREEN)"' "0" "${test_color_bg_green}" "Check the background color green."
+    assert_run 'term::printf "%(YELLOW)"' "0" "${test_color_bg_yellow}" "Check the background color yellow."
+    assert_run 'term::printf "%(BLUE)"' "0" "${test_color_bg_blue}" "Check the background color blue."
+    assert_run 'term::printf "%(MAGENTA)"' "0" "${test_color_bg_magenta}" "Check the background color magenta."
+    assert_run 'term::printf "%(CYAN)"' "0" "${test_color_bg_cyan}" "Check the background color cyan."
+    assert_run 'term::printf "%(WHITE)"' "0" "${test_color_bg_white}" "Check the background color white."
+    assert_run 'term::printf "%(BRIGHTBLACK)"' "0" "${test_color_bg_brightblack}" "Check the background color brightblack."
+    assert_run 'term::printf "%(BRIGHTRED)"' "0" "${test_color_bg_brightred}" "Check the background color brightred."
+    assert_run 'term::printf "%(BRIGHTGREEN)"' "0" "${test_color_bg_brightgreen}" "Check the background color brightgreen."
+    assert_run 'term::printf "%(BRIGHTYELLOW)"' "0" "${test_color_bg_brightyellow}" "Check the background color brightyellow."
+    assert_run 'term::printf "%(BRIGHTBLUE)"' "0" "${test_color_bg_brightblue}" "Check the background color brightblue."
+    assert_run 'term::printf "%(BRIGHTMAGENTA)"' "0" "${test_color_bg_brightmagenta}" "Check the background color brightmagenta."
+    assert_run 'term::printf "%(BRIGHTCYAN)"' "0" "${test_color_bg_brightcyan}" "Check the background color brightcyan."
+    assert_run 'term::printf "%(BRIGHTWHITE)"' "0" "${test_color_bg_brightwhite}" "Check the background color brightwhite."
 }
 
 # Test short codes..
@@ -200,22 +266,22 @@ test::short_codes(){
     assert_run 'term::printf "%[z]"' "0" "${test_attr_cnorm}" "Check the short code (z) Show the cursor."
     assert_run 'term::printf "%[o]"' "0" "${test_attr_op}" "Check the short code (o) Original colors."
 
-    assert_run 'term::printf "%[k]"' "0" "$(tput setaf 0)" "Check the foreground color black."
-    assert_run 'term::printf "%[r]"' "0" "$(tput setaf 1)" "Check the foreground color red."
-    assert_run 'term::printf "%[g]"' "0" "$(tput setaf 2)" "Check the foreground color green."
-    assert_run 'term::printf "%[y]"' "0" "$(tput setaf 3)" "Check the foreground color yellow."
-    assert_run 'term::printf "%[b]"' "0" "$(tput setaf 4)" "Check the foreground color blue."
-    assert_run 'term::printf "%[m]"' "0" "$(tput setaf 5)" "Check the foreground color magenta."
-    assert_run 'term::printf "%[c]"' "0" "$(tput setaf 6)" "Check the foreground color cyan."
-    assert_run 'term::printf "%[w]"' "0" "$(tput setaf 7)" "Check the foreground color white."
+    assert_run 'term::printf "%[k]"' "0" "${test_color_fg_black}" "Check the foreground color black."
+    assert_run 'term::printf "%[r]"' "0" "${test_color_fg_red}" "Check the foreground color red."
+    assert_run 'term::printf "%[g]"' "0" "${test_color_fg_green}" "Check the foreground color green."
+    assert_run 'term::printf "%[y]"' "0" "${test_color_fg_yellow}" "Check the foreground color yellow."
+    assert_run 'term::printf "%[b]"' "0" "${test_color_fg_blue}" "Check the foreground color blue."
+    assert_run 'term::printf "%[m]"' "0" "${test_color_fg_magenta}" "Check the foreground color magenta."
+    assert_run 'term::printf "%[c]"' "0" "${test_color_fg_cyan}" "Check the foreground color cyan."
+    assert_run 'term::printf "%[w]"' "0" "${test_color_fg_white}" "Check the foreground color white."
 
-    assert_run 'term::printf "%[K]"' "0" "$(tput setab 0)" "Check the background color black."
-    assert_run 'term::printf "%[R]"' "0" "$(tput setab 1)" "Check the background color red."
-    assert_run 'term::printf "%[G]"' "0" "$(tput setab 2)" "Check the background color green."
-    assert_run 'term::printf "%[Y]"' "0" "$(tput setab 3)" "Check the background color yellow."
-    assert_run 'term::printf "%[B]"' "0" "$(tput setab 4)" "Check the background color blue."
-    assert_run 'term::printf "%[M]"' "0" "$(tput setab 5)" "Check the background color magenta."
-    assert_run 'term::printf "%[C]"' "0" "$(tput setab 6)" "Check the background color cyan."
-    assert_run 'term::printf "%[W]"' "0" "$(tput setab 7)" "Check the background color white."
+    assert_run 'term::printf "%[K]"' "0" "${test_color_bg_black}" "Check the background color black."
+    assert_run 'term::printf "%[R]"' "0" "${test_color_bg_red}" "Check the background color red."
+    assert_run 'term::printf "%[G]"' "0" "${test_color_bg_green}" "Check the background color green."
+    assert_run 'term::printf "%[Y]"' "0" "${test_color_bg_yellow}" "Check the background color yellow."
+    assert_run 'term::printf "%[B]"' "0" "${test_color_bg_blue}" "Check the background color blue."
+    assert_run 'term::printf "%[M]"' "0" "${test_color_bg_magenta}" "Check the background color magenta."
+    assert_run 'term::printf "%[C]"' "0" "${test_color_bg_cyan}" "Check the background color cyan."
+    assert_run 'term::printf "%[W]"' "0" "${test_color_bg_white}" "Check the background color white."
 }
 
