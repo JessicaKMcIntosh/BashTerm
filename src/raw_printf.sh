58520bdea061e4f342b7fd69be8667eda3936a74
Jessica K McIntosh <>
2026-01-24 12:14:01 -0800

    Properly detect the AWK command.

--- src/raw_printf.sh
+++ src/raw_printf.sh
@@ -1,5 +1,16 @@
 # Configuration.
-_TERM_AWK_COMMAND="awk"
+declare -g _TERM_AWK_COMMAND="awk"
+term::printf_setup(){
+    local awk_command
+    # mawk is generally faster than gawk.
+    for awk_command in {m,}awk gawk ; do
+        if command -v "${awk_command}" > /dev/null ; then
+            declare -g _TERM_AWK_COMMAND="${awk_command}"
+            break
+        fi
+    done
+}
+term::printf_setup
 #_TERM_AWK_COMMAND="gawk --lint" # For development.
 
 # Find the file printf.awk.
