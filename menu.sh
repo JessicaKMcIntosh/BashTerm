908a64a49ab6fb9865eeff3d08ca733b18038530
Jessica K McIntosh <>
2026-01-24 21:48:10 -0800

    Typo.

--- menu.sh
+++ menu.sh
@@ -264,7 +264,7 @@ if [[ "${0}" == "${BASH_SOURCE[0]}" ]] ; then
         declare example_file="${0##*/}"
         example_file="${example_file%.*}"
         echo "For an example try:"
-        menu "./examples/%s_example.sh\n" "${example_file}"
+        printf "./examples/%s_example.sh\n" "${example_file}"
         echo ""
         echo "For help:"
         echo "${0} -h"
