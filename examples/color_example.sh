be753d5511adb22476c315b19e3bb27cfc17bed4
Jessica K McIntosh <>
2026-01-30 20:44:08 -0800

    The color example file was skipped.

--- examples/color_example.sh
+++ examples/color_example.sh
@@ -8,6 +8,13 @@
 
 # Example of using the color library.
 
+# This requires bash version 4.
+if ((BASH_VERSINFO[0] < 4)); then
+    echo "This script requires Bash 4 or later."
+    echo "Current version: ${BASH_VERSION}"
+    exit 1
+fi
+
 # Load the libraries.
 declare -a library_list=("attr.sh" "color.sh")
 find_library() {
